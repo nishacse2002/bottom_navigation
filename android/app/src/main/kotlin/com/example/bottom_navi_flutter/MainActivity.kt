8d5e46ef5872afb3418074b0e0a1976614e27392
nishacse2002 <p.nishandhini2002@gmail.com>
2024-02-14 18:54:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/bottom_navi_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bottom_navi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:bottom_navi_flutter/home.dart';
+import 'package:bottom_navi_flutter/search.dart';
+import 'package:bottom_navi_flutter/settings.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomeApp(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomeApp extends StatefulWidget {
+  const MyHomeApp({super.key});
+
+  @override
+  State<MyHomeApp> createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  int _currentIndex = 0;
+  final List tabs = [
+    Home(),
+    Search(),
+    Setting(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'settings'),
+        ],
+      ),
+    );
+  }
+}
